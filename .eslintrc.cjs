eb7e353bda7ba9c8daca922660f2b61c6bafcc5b
xiangyuanhui <yuanhui.xiang@gientech.com>
2023-07-17 22:02:16 +0800

    创建模板

---
+++ .eslintrc.cjs
@@ -0,0 +1,83 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'vue'],
+  /**
+   * 'off'或0:关闭规格校验
+   * 'warn'或1: 警告提示，代码可正常运行
+   * 'error'或2: 错误提示，代码不能运行
+   * ***/
+  rules: {
+    'no-var': 'error',
+    'no-multiple-empty-lines': ['warn', { max: 1 }], // 不允许多个空格
+    'no-unexpected-multiline': 'error', // 禁止多余的空行
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // typeScript
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': 'error',
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/object-curly-spacing': 'off',
+    'no-useless-escape': 'off',
+
+    'vue/attribute-hyphenation': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 10,
+        multiline: 1,
+      },
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+          normal: 'any',
+          component: 'any',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/html-indent': 'off',
+  },
+}
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+createApp(App).mount('#app')
+const b = 1
+
+const test = () => {
+  console.log('zehshi')
+}
+
+test()
